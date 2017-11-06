c75442be395abb555dcc7c850b352dd1ed2ca976
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-11-06 15:10:38 +0800

    zhaojian

---
+++ application/index/controller/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;
+class Index extends Base
+{
+    /**
+     * 构造方法
+     */
+    public function _auto()
+    {
+
+    }
+
+    /**
+     * 默认方法
+     */
+    public function index()
+    {
+        // Get
+        if ($this->request->isGet()) {
+            // 渲染模板输出
+            return $this->fetch('index');
+        }
+    }
+}
